c5058237f60345bd5d7abec8e4c18f8198ef2e5a
María Jesús López Salmerón <mjls130598@gmail.com>
2021-12-20 18:52:32 +0100

    Suma

---
+++ testapp/views.py
@@ -0,0 +1,40 @@
+import json
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def get_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                            'method': request.method,
+                            'path': request.path,
+                            'params': request.GET,
+                            'headers': dict(request.headers)}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+
+def suma(request, a, b):
+    response = {
+        'resultado': int(a) + int(b)
+    }
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+@csrf_exempt
+def post_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                            'method': request.method,
+                            'path': request.path,
+                            'params': request.GET,
+                            'headers': dict(request.headers),
+                            'body': request.body.decode()}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+@csrf_exempt
+def post_suma(request):
+    body = json.loads(request.body.decode())
+    a = body["a"]
+    b = body["b"]
+
+    response = {
+        'resultado': int(a) + int(b)
+    }
+    return JsonResponse(response, safe=False,  json_dumps_params={'indent': 2})
--- webproject/urls.py
+++ webproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import testapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get_example/', testapp.views.get_example),
+    path('suma/<a>+<b>', testapp.views.suma),
+    path('post_example/', testapp.views.post_example),
+    path('post_suma', testapp.views.post_suma),
 ]

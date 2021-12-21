65e73c62906b3afac3f13e48ba32f4b70a144765
María Jesús López Salmerón <mjls130598@gmail.com>
2021-12-21 18:31:10 +0100

    Settings

--- webproject/settings.py
+++ webproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('.secret_key', 'r', encoding='utf-8').read()
+SECRET_KEY = 'django-insecure-l^kp5^el)_k&2ai6wh5z!ht())_6s678w6#5%vx2_ryf)q$=lv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
